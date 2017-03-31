f52a920f67c4e6814dc654dc4917a5d40eeb40db
chenJz1012 <597160667@qq.com>
2017-03-31 21:26:39 +0800

    fix

--- src/js/login.js
+++ src/js/login.js
@@ -1,10 +1,14 @@
-/**
- * Created by chenguojun on 8/10/16.
- */
-
+;
 (function ($, window, document, undefined) {
+    var vkey = "bi_" + new Date().getTime() + "_" + Math.floor(Math.random() * 10);
+
     function initLogin() {
-        $('#username,#password').bind('keypress', function (event) {
+        $("#captcha_img").attr("src", App.href + "/api/noneAuth/captcha?vkey=" + vkey);
+        $("#captcha_a").on("click", function () {
+            vkey = "bi_" + new Date().getTime() + "_" + Math.floor(Math.random() * 10);
+            $("#captcha_img").attr("src", App.href + "/api/noneAuth/captcha?vkey=" + vkey + "&s=" + new Date().getTime());
+        });
+        $('#username,#password,#vcode').bind('keypress', function (event) {
             if (event.keyCode == "13") {
                 login();
             }
@@ -13,13 +17,16 @@
     }
 
     var login = function () {
-        if ($("#username").val() === "" || $("#username").val() == "password") {
-            alert("登录名或密码不能为空!")
+        if ($("#username").val() == "" || $("#username").val() == "" || $("#vcode").val() == "") {
+            alert("登录名,密码,验证码不能为空!");
+            return;
         }
         var fields = JSON.stringify(
             {
                 "username": $("#username").val(),
-                "password": $("#password").val()
+                "password": $("#password").val(),
+                "vcode": $("#vcode").val(),
+                "vkey": vkey
             });
         $.ajax({
             type: 'POST',
@@ -28,15 +35,12 @@
             dataType: "json",
             data: fields,
             success: function (result) {
-                if (result.code == 200) {
+                if (result.code === 200) {
                     $.cookie('tc_t', result.token, {expires: 7});
                     window.location.href = App.href;
                 } else {
                     alert(result.message);
                 }
-            },
-            error: function (err) {
-
             }
         });
     };
