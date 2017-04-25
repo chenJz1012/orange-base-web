0ebd71daa067c779e702bf555786ccce0c98caf8
chenJz1012 <597160667@qq.com>
2017-04-25 13:43:32 +0800

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -17,25 +17,25 @@
     App.index = {
         page: function (title) {
             App.content.empty();
+            console.info(1);
             App.title(title);
+            console.info(2);
             var content = $('<div class="panel-body" >' +
                 '<div class="row">' +
                 '<div class="col-md-12" id="index_grid"></div> ' +
                 '</div> ' +
                 '</div>');
             App.content.append(content);
-            App.index.initEvents();
+            console.info(3);
+            initEvents();
         }
     };
     /**
      * 初始化事件
      */
-    App.index.initEvents = function () {
+    var initEvents = function () {
         var gridOptions = {
             url: App.href + "/api/sys/role/roleUserCount",
-            beforeSend: function (request) {
-                request.setRequestHeader("X-Auth-Token", App.token);
-            },
             contentType: "chart-bar",
             showContentType: true,
             contentTypeItems: "table,list,chart-bar,chart-pie",
@@ -64,7 +64,7 @@
                 }
             ]
         };
-        var grid = window.App.content.find("#index_grid").orangeGrid(gridOptions);
+        var grid = App.content.find("#index_grid").orangeGrid(gridOptions);
     };
 
 })(jQuery, window, document);
