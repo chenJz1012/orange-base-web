3fa8d80d27464811452a0bf4d8a65f7851e0ba5d
chenJz1012 <597160667@qq.com>
2017-04-25 15:06:29 +0800

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -21,8 +21,13 @@
             var content = $(
                 '<div class="panel-body" >' +
                 '<div class="row">' +
-                '<div class="col-md-12" id="index_grid">' +
-
+                '<div class="col-md-12">' +
+                '<div class="panel panel-default">' +
+                '<div class="panel-heading">' +
+                '<i class="fa fa-bar-chart-o fa-fw"></i>学生' +
+                '</div>' +
+                '<div class="panel-body" id="index_grid"></div>' +
+                '</div>' +
                 '</div> ' +
                 '</div> ' +
                 '</div>')
